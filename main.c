ba3831712494d59874b72162030e3868c0487be6
tOpDeveLOpeR-pAs <71667213+tOpDeveLOpeR-pAs@users.noreply.github.com>
2020-09-26 20:52:10 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,21 +1,24 @@
 ﻿/* QUADRATIC EQUATION */
+#define _CRT_SECURE_NO_WARNINGS 
 
 #include <stdio.h>
 #include "square.h"
-
+#include <Windows.h>
 // ====================================================================
 
 void CheckDouble(double*, const char[]); // Checking the correctness of the entered data
+void TestSolver(); // Programm testing
 
 // ====================================================================
 
 int main() {
+	SetConsoleOutputCP(1251);
 
 	printf("# Welcome, this is square equation solver.\n# (c) Pavlov Sasha 2020 \n");
 	printf("\n# Square equation: a * x^2 + b * x + c = 0\n\n");
-	
+
 	double cf1 = 0, cf2 = 0, cf3 = 0;
-	
+
 	CheckDouble(&cf1, "Enter a: ");
 	CheckDouble(&cf2, "Enter b: ");
 	CheckDouble(&cf3, "Enter c: ");
@@ -37,10 +40,10 @@ int main() {
 		printf("\nThis equation has two roots.\nx1 = %lf\nx2 = %lf\n", root1, root2);
 		break;
 
-	case SS_INF_ROOTS: 
+	case SS_INF_ROOTS:
 		printf("\nThis equation has infinitely many roots.\n");
 		break;
-	
+
 	case DISC_LESS_ZERO:
 		printf("\nThis equation has two roots in  complex plane.\n");
 		break;
@@ -49,6 +52,8 @@ int main() {
 		printf("main(): ERROR: numRoots = %d\n", numRoots);
 		return 1;
 	}
+
+	TestSolver();
 
 	return 0;
 }
@@ -72,4 +77,27 @@ void CheckDouble(double* d_num, const char prompt[]) {
 	}
 }
 
+// ======================================================================
 
+void TestSolver() {
+	printf("\nTest run ...\n");
+
+	double arr_coeff[] = { 0, 0, 0, -1, 2, 1, 23.4, -22.5745, 34, 0, 1e-7, 2.4756 };
+	double arr_results[] = { SS_INF_ROOTS, 2, DISC_LESS_ZERO, 1 };
+	double root1_ch = 0, root2_ch = 0;
+	int check_numR;
+
+	for (int i = 0; i < 4; ++i) {
+		check_numR = SolveSquare(arr_coeff[i * 3], arr_coeff[i * 3 + 1], arr_coeff[i * 3 + 2], &root1_ch, &root2_ch);
+
+		if (check_numR == arr_results[i])
+		{
+			printf("\nTest №%d passed.\n", i + 1);
+		}
+		else
+		{
+			printf("\nTest №%d failed.\n", i + 1);
+			printf("Final results:\n\tNumber of roots = %d\n\troot1 = %lg\n\troot2 = %lg", check_numR, &root1_ch, &root2_ch);
+		}
+	}
+}
