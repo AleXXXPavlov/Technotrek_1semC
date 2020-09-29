696a5493f002f41a46ac0dd43d81d7c11cec562a
tOpDeveLOpeR-pAs <71667213+tOpDeveLOpeR-pAs@users.noreply.github.com>
2020-09-30 01:57:02 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -2,28 +2,28 @@
 
 #include "square.h"
 #include "unittest.h"
+#include "check.h"
 
 #include <stdio.h>
 
-// --------------------------------------------------------------------
-//! Function for checking the correctness of the input
-//! 
-//! @param [in] d_num - Pointer of the n-st coefficient 
-//! @param [in] prompt[] - Output information
-// --------------------------------------------------------------------
-void CheckDouble(double*, const char[]);
+// --------------------------------------------------
+//! Function for displaying a greeting
+// --------------------------------------------------
+void greeting();
 
-// --------------------------------------------------------------------
-//! Function for testing the programm
-//! Checking all return values of a function SolveSquare
-// --------------------------------------------------------------------
-void TestSolver();
+// --------------------------------------------------
+//! Function for displaying the result of the program
+//!
+//! @param [in]  pointer to the first coefficient
+//! @param [in]  pointer to the second coefficient
+//! @param [in]  pointer to the third coefficient
+//!
+// --------------------------------------------------
+void result(double*, double*, double*);
 
 int main() {
 
-	/* Input of initial data */
-	printf("# Welcome, this is square equation solver.\n# (c) Pavlov Sasha 2020 \n");
-	printf("\n# Square equation: a * x^2 + b * x + c = 0\n\n");
+	greeting();
 
 	double cf1 = 0, cf2 = 0, cf3 = 0;
 
@@ -32,9 +32,19 @@ int main() {
 	CheckDouble(&cf2, "Enter b: ");
 	CheckDouble(&cf3, "Enter c: ");
 
-	/* Solution implementation */
+	result(&cf1, &cf2, &cf3);
+
+	return 0;
+}
+
+void greeting() {
+	printf("# Welcome, this is square equation solver.\n# (c) Pavlov Sasha 2020 \n");
+	printf("\n# Square equation: a * x^2 + b * x + c = 0\n\n");
+}
+
+void result(double* ptr1, double* ptr2, double* ptr3) {
 	double root1 = 0, root2 = 0;
-	int numRoots = SolveSquare(cf1, cf2, cf3, &root1, &root2);
+	int numRoots = SolveSquare(*ptr1, *ptr2, *ptr3, &root1, &root2);
 
 	switch (numRoots)
 	{
@@ -60,25 +70,5 @@ int main() {
 
 	default:
 		printf("main(): ERROR: numRoots = %d\n", numRoots);
-		return 1;
-	}
-
-	return 0;
-}
-
-void CheckDouble(double* d_num, const char prompt[]) {
-	while (1) {
-		printf("%s", prompt);
-
-		if (scanf("%lg", d_num) != 1)
-		{
-			printf("Input Error: Make sure that the endered data is correct.\n");
-
-			scanf("%*[^\n]");
-		}
-		else
-		{
-			break;
-		}
 	}
 }
